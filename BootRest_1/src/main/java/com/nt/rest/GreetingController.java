16a04421b6c0f1fd08a773de1a2df5703613b232
Dilli850 <dilli.baipalli@gmail.com>
2024-07-28 14:05:29 +0530

    commit suacasdf

---
+++ BootRest_1/src/main/java/com/nt/rest/GreetingController.java
@@ -0,0 +1,13 @@
+package com.nt.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "Hello, World!";
+    }
+}

0466c56e8a5e5215fcafc0311989a8423c8bbce1
Dilli850 <dilli.baipalli@gmail.com>
2024-07-28 12:18:36 +0530

    initial commit

---
+++ BootRest_1/src/main/java/com/nt/rest/Wish.java
@@ -0,0 +1,20 @@
+package com.nt.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Wish {
+	
+	
+	@GetMapping("/actor")
+	public ResponseEntity<String> wishmessage()
+	{
+		return new ResponseEntity<>("mrng",HttpStatus.OK);
+	}
+
+}
